face0e619d3b719726d32d0d4087fd8177664fdc
George T. Seese <gtseese@outlook.com>
2020-01-14 16:58:01 -0700

    Bugfix: fix missing check for .under_test is True for Agitation drives

--- fioAutomater.py
+++ fioAutomater.py
@@ -2247,6 +2247,7 @@ def parse_and_save_wlg_results(result_location, db_file, table_name, fio_result_
             if db_insertion_string[-1] is ',':
                 db_insertion_string = db_insertion_string[:-1]
 
+            # TODO: This is broken on ISP mode; row_number is wildly off
             # only update the job being parsed for the current workload
             db_insertion_string = db_insertion_string + ' WHERE id=%s AND WLID=%s AND name="%s";' % \
                                   (row_number, wl_number, job_name)
